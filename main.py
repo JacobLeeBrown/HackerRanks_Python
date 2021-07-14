b2bc4de43aa97c16caf11b82bd475badd6d1d714
Jacob Brown <jlbrown5@illinois.edu>
2021-07-14 08:14:02 -0500

    Initial python files

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)10s - %(levelname)7s - %(message)s', level=logging.DEBUG
+)
+
+
+def main():
+
+    logging.info("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
