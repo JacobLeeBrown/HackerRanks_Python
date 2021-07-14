28eb09be7e5395a1f4a54afefec491b5568c55d4
Jacob Brown <jlbrown5@illinois.edu>
2021-07-14 09:10:49 -0500

    Problem skeletons

---
+++ hackerranks/longest_substring.py
@@ -0,0 +1,9 @@
+# Given a string s
+# Return the length of the longest substring without repeating characters.
+
+def longest_substring_naive(s):
+    return 0
+
+
+def longest_substring_optimal(s):
+    return 0
---
+++ hackerranks/next_permutation.py
@@ -0,0 +1,8 @@
+# Given a list of numbers,
+# Return the same list rearranged into the lexicographically next greater permutation. Ex: [1, 3, 2] -> [2, 1, 3]
+#   If such an arrangement is not possible (list already lexicographically greatest order), then return lowest order
+#       Ex: [3, 2, 1] -> [1, 2, 3]
+# The replacement must be in place and use only constant extra memory.
+
+def next_permutation(num_list):
+    pass
