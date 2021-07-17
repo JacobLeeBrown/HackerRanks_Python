ec0f2be3af40f02c338cf86089660347c05984c4
Jacob Brown <jlbrown5@illinois.edu>
2021-07-16 19:30:57 -0500

    Completed my_atoi with tests and comments

---
+++ hackerranks/myatoi/my_atoi.py
@@ -0,0 +1,50 @@
+
+def my_atoi(s):
+    """ Convert string to 32-bit signed integer following algorithm of C's atoi
+    function.
+
+    Parameters
+    ----------
+    s : str
+        String to parse
+
+    Returns
+    -------
+    int
+        32-bit signed integer value of parsed `s`
+    """
+
+    # Trim leading spaces
+    s = s.lstrip(' ')
+
+    if len(s) == 0:
+        return 0
+
+    # Read first character if it is a sign
+    sign_multiplier = 1
+    if s[0] in {'-', '+'}:
+        if s[0] == '-':
+            sign_multiplier = -1
+        s = s[1:]
+        if len(s) == 0:
+            return 0
+
+    # Read characters until first non-digit
+    for i, char in enumerate(s):
+        if char not in "1234567890":
+            s = s[:i]
+            break
+
+    if len(s) == 0:
+        return 0
+
+    # Can now parse string to int (can only be positive)
+    s_int = int(s)
+
+    # Clamp to signed 32-bit bounds
+    max_int = 2**31
+    max_int = max_int if (sign_multiplier == -1) else (max_int - 1)
+    s_int = min(s_int, max_int)
+
+    # Return signed result
+    return s_int * sign_multiplier
---
+++ hackerranks/myatoi/my_atoi_test.py
@@ -0,0 +1,58 @@
+import unittest
+import my_atoi
+
+
+class TestLongestSubstring(unittest.TestCase):
+
+    def test_normal_positive_int(self):
+        s = "1234"
+        self.assertEqual(1234, my_atoi.my_atoi(s))
+
+    def test_ignores_leading_spaces(self):
+        s = "       1234"
+        self.assertEqual(1234, my_atoi.my_atoi(s))
+
+    def test_reads_signs(self):
+        s = "+1234"
+        self.assertEqual(1234, my_atoi.my_atoi(s))
+
+        s = "-1234"
+        self.assertEqual(-1234, my_atoi.my_atoi(s))
+
+    def test_ignores_everything_after_first_non_digit(self):
+        s = "1234 5678 9000"
+        self.assertEqual(1234, my_atoi.my_atoi(s))
+
+        s = "1234asdf1234"
+        self.assertEqual(1234, my_atoi.my_atoi(s))
+
+    def test_valid_zero_strings(self):
+        s = "0"
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+        s = "    -0000"
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+        s = "    +0000zyxw"
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+    def test_zero_edge_cases(self):
+        s = ""
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+        s = "   +"
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+        s = "   -asdf"
+        self.assertEqual(0, my_atoi.my_atoi(s))
+
+    def test_32_bit_signed_bounds(self):
+        s = "2147483648"  # 2**31 = upper_bound + 1 -> should return 2**31-1
+        self.assertEqual(2147483647, my_atoi.my_atoi(s))
+
+        s = "-2147483649"  # -2**31-1 = lower_bound - 1 -> should return -2**31
+        self.assertEqual(-2147483648, my_atoi.my_atoi(s))
+
+
+if __name__ == '__main__':
+    unittest.main()
