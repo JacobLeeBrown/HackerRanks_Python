7e1fc306e81cbbaccddbb7120443bb56a1af9752
Jacob Brown <jlbrown5@illinois.edu>
2021-07-17 13:41:38 -0500

    Cleaned up imports for unit tests

--- hackerranks/longestsubstring/longest_substring_test.py
+++ hackerranks/longestsubstring/longest_substring_test.py
@@ -1,5 +1,5 @@
 import unittest
-import longest_substring
+from longest_substring import longest_substring
 
 
 class TestLongestSubstring(unittest.TestCase):
@@ -8,73 +8,73 @@ class TestLongestSubstring(unittest.TestCase):
         test_str = ""
         expected = 0
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_one_char_returns_one(self):
         test_str = "1"
         expected = 1
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_no_repeats_returns_length(self):
         test_str = "abcdefghij"
         expected = 10
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_repeats_at_bounds(self):
         test_str = "1234561111"
         expected = 6
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
         test_str = "0004567890"
         expected = 7
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_interwoven_repeats(self):
         test_str = "1234561121"
         expected = 6
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
         test_str = "0004564890"
         expected = 6
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_only_repeats_returns_one(self):
         test_str = "9999999999"
         expected = 1
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
         test_str = "jjjjjjjjjj"
         expected = 1
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
     def test_tied_best_substrings(self):
         test_str = "1234123"
         expected = 4
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
         test_str = "111a2b3c4d1a22"
         expected = 8
 
-        actual = longest_substring.longest_substring(test_str)
+        actual = longest_substring(test_str)
         self.assertEqual(expected, actual)
 
 
