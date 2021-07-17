d1ccd3bd78cfc3e7c93a1e7f2308df164efd63c4
Jacob Brown <jlbrown5@illinois.edu>
2021-07-17 14:03:06 -0500

    Next permutation needs some work

--- hackerranks/nextpermutation/next_permutation_test.py
+++ hackerranks/nextpermutation/next_permutation_test.py
@@ -4,7 +4,7 @@ from next_permutation import next_permutation
 
 class MyTestCase(unittest.TestCase):
 
-    def test_happy_path(self):
+    def test_list_in_least_permutation(self):
         actual = [1, 2, 3]
 
         # Should not have to reassign since operation is in-place
@@ -50,7 +50,18 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
+    def test_unordered_list(self):
+        # TODO: Currently fails
+        actual = [5, 3, 7, 6, 4]
+
+        next_permutation(actual)
+
+        expected = [5, 4, 3, 6, 7]
+
+        self.assertEqual(expected, actual)
+
     def test_list_with_duplicates(self):
+        # TODO: Currently fails
         actual = [4, 3, 4, 6, 4]
 
         next_permutation(actual)
